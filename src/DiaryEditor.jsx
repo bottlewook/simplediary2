74c7f6395f6a229fe9319b31f7297121088594fb
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-07-02 22:02:51 +0900

    useState

---
+++ src/DiaryEditor.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장완료");
+  };
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+          placeholder="저자를 입력해주세요."
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+          placeholder="본문을 입력해주세요."
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
