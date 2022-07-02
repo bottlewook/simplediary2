1baaff1aa499d47bf23f0d3cc5c499eed054ff18
임병욱 <imbyeong-ug@imbyeong-ug-ui-MacBookPro.local>
2022-07-02 22:29:49 +0900

    useRef

--- src/DiaryEditor.jsx
+++ src/DiaryEditor.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,7 +18,15 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
     alert("저장완료");
   };
   return (
@@ -23,6 +34,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -31,6 +43,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
@@ -38,6 +51,7 @@ const DiaryEditor = () => {
         />
       </div>
       <div>
+        <span>감정점수 : </span>
         <select
           name="emotion"
           value={state.emotion}
